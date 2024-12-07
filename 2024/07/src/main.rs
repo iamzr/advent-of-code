d3252496644f869e0c373dab60fb87a0298e53a9
Zia Rehman <iamziarehman@gmail.com>
2024-12-07 14:38:22 +0000

    2024-07 Part 2

--- 2024/07/src/main.rs
+++ 2024/07/src/main.rs
@@ -30,7 +30,7 @@ fn main() {
     for (target, l) in values {
         // let l_rv = l.iter().copied().rev().collect::<Vec<i64>>();
 
-        let v = find(target, 0, 0, &l);
+        let v = find(target, 0, 0, &l, true);
         println!("For target {} we found {}", target, v);
 
         if v {
@@ -42,7 +42,7 @@ fn main() {
 }
 
 /// For a given target there's two things you can do
-fn find(target: i64, curr: i64, idx: usize, l: &Vec<i64>) -> bool {
+fn find(target: i64, curr: i64, idx: usize, l: &Vec<i64>, concat: bool) -> bool {
     if curr > target {
         // println!("greater than target {}, curr {}", target, curr);
         return false
@@ -61,11 +61,27 @@ fn find(target: i64, curr: i64, idx: usize, l: &Vec<i64>) -> bool {
     let n= l[idx];
     // println!("{}", n);
 
-    let check_1 = find(target, curr + n, idx +1, l);
+    let check_1 = find(target, curr + n, idx +1, l, concat);
     // println!("Additive path {}", check_1);
-    let check_2 = find(target, curr * n, idx + 1, l);
+    let check_2 = find(target, curr * n, idx + 1, l, concat);
     // println!("Multiplicative path {}", check_2);
+    let check_3 = find(target, curr * 10_i64.pow(n.ilog10() + 1) + n, idx + 1, l, concat);
 
-    check_1 || check_2
+    check_1 || check_2 || check_3
+
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_concating_op() {
+        let n1 :i64 = 123;
+        let n2 :i64 = 456;
+
+        assert_eq!(123456, n1 * 10_i64.pow(n2.ilog10() + 1) + n2)
+    }
 
 }
