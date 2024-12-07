d715af5b9a1f775007849fd0d464c8d31652a096
Zia Rehman <iamziarehman@gmail.com>
2024-12-07 16:44:14 +0000

    2024-07 tidied up solution

--- 2024/07/src/main.rs
+++ 2024/07/src/main.rs
@@ -1,48 +1,84 @@
-use std::fs;
+use std::{fs, ops, str::FromStr};
 
 fn main() {
-    let file_path = "./input.txt";
+    let file_path = "./test.txt";
 
     let contents = fs::read_to_string(file_path).unwrap();
 
-    let values = contents
-        .split("\n")
+    let values = contents.split("\n")
         .collect::<Vec<&str>>()
         .into_iter()
-        .map(|s| s.split(":").collect::<Vec<&str>>())
-        .map(|row| {
-            println!("{:?}", row);
-            (
-                row[0].parse::<i64>().expect("Invalid Number"),
-                row[1]
-                    .split_whitespace()
-                    .map(|num| num.parse::<i64>().expect("Invalid Number"))
-                    .collect::<Vec<i64>>(),
-            )
-        })
-        .collect::<Vec<(i64, Vec<i64>)>>();
+        .map(|row| row.parse::<Equation>().expect("Parsing issue"))
+        .collect::<Vec<Equation>>();
 
     println!("{:?}", values);
 
-    let mut ans = 0;
-
-
-    for (target, l) in values {
-        // let l_rv = l.iter().copied().rev().collect::<Vec<i64>>();
-
-        let v = find(target, 0, 0, &l, true);
-        println!("For target {} we found {}", target, v);
-
-        if v {
-            ans += target;
-        }
-    }
+    let ans = part_one(values);
 
     println!("Final answer: {}", ans);
 }
 
+#[derive(Debug, PartialEq, Eq)]
+struct ParseEquationError;
+
+#[derive(Debug)]
+struct Equation {
+    test: i64,
+    operands: Vec<i64>
+}
+
+impl FromStr for Equation {
+    type Err = ParseEquationError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let row = s.split(":").collect::<Vec<&str>>();
+        println!("{:?}", row);
+
+        let test = row[0].parse::<i64>().map_err(|_| ParseEquationError)?;
+
+        let operands= match row[1]
+                    .split_whitespace()
+                    .map(|num| Ok(num.parse::<i64>().map_err(|_| ParseEquationError)?))
+                    .collect::<Result<Vec<i64>, ParseEquationError>>() {
+                        Ok(v) => v,
+                        Err(_e) => return Err(_e),
+                    };
+
+        Ok(Equation {
+                test: test ,
+                operands:operands,
+        })
+
+    }
+}
+
+fn part_one (equations: Vec<Equation>) -> i64 {
+    let operations = vec![
+        ops::Add::add,
+        ops::Mul::mul,
+        concat
+    ];
+
+    let mut ans = 0;
+
+    for equation in equations {
+        let v = find(equation.test, 0, 0, &equation.operands, &operations);
+        println!("For target {} we found {}", equation.test, v);
+
+        if v {
+            ans += equation.test;
+        }
+    }
+
+    ans
+}
+
+fn concat(a: i64, b: i64) -> i64 {
+    a * 10_i64.pow(b.ilog10() + 1) + b
+}
+
 /// For a given target there's two things you can do
-fn find(target: i64, curr: i64, idx: usize, l: &Vec<i64>, concat: bool) -> bool {
+fn find(target: i64, curr: i64, idx: usize, l: &Vec<i64>, operations: &Vec<fn(i64, i64) -> i64>) -> bool {
     if curr > target {
         // println!("greater than target {}, curr {}", target, curr);
         return false
@@ -61,13 +97,13 @@ fn find(target: i64, curr: i64, idx: usize, l: &Vec<i64>, concat: bool) -> bool 
     let n= l[idx];
     // println!("{}", n);
 
-    let check_1 = find(target, curr + n, idx +1, l, concat);
-    // println!("Additive path {}", check_1);
-    let check_2 = find(target, curr * n, idx + 1, l, concat);
-    // println!("Multiplicative path {}", check_2);
-    let check_3 = find(target, curr * 10_i64.pow(n.ilog10() + 1) + n, idx + 1, l, concat);
+    let mut res = false;
 
-    check_1 || check_2 || check_3
+    for op in operations {
+       res = res || find(target, op(curr, n), idx + 1, l, operations);
+    }
+
+    res
 
 }
 
@@ -81,7 +117,7 @@ mod tests {
         let n1 :i64 = 123;
         let n2 :i64 = 456;
 
-        assert_eq!(123456, n1 * 10_i64.pow(n2.ilog10() + 1) + n2)
+        assert_eq!(123456, concat(n1, n2))
     }
 
 }
