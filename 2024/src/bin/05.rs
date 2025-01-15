54ef58cfe3c3eacfbca91e89e5d77a48448c0c03
Zia Rehman <iamziarehman@gmail.com>
2025-01-15 20:30:21 +0000

    2024-17 Part1: Used Comparer function instead. Part 2: Completed.

--- 2024/src/bin/05.rs
+++ 2024/src/bin/05.rs
@@ -1,66 +1,66 @@
-use core::panic::PanicInfo;
+use std::cmp::Ordering;
 
 advent_of_code::solution!(5);
 
 fn parse(input: &str) -> (Vec<Vec<usize>>, Vec<Vec<usize>>) {
 
-    if let Some((checks, updates))= input.split_once("\n\n") {
-        let mut rules = vec![Vec::new(); 100];
-
-        for check in checks.lines(){
-            if let Some((x,y)) = check.split_once("|") {
-                    let x = x.parse::<usize>().expect("Failed to parse number");
-                    let y = y.parse::<usize>().expect("Failed to parse number");
-
-                    rules[x].push(y);
-                };
-            // panic!("Failed to parse");
-            }
-            
-        let updates = updates.lines().map(|s| s.split(",").map(|x| x.parse::<usize>().unwrap()).collect::<Vec<usize>>()).collect::<Vec<Vec<usize>>>();
-
-        return (rules, updates);
-    }
-
-    panic!("Failed to parse");
-}
-
-
-fn check_updates(rules: &Vec<Vec<usize>>, updates: Vec<Vec<usize>>) -> usize {
-    let mut sum = 0;
-    'update: for update in updates {
-        for (index, value) in update.iter().enumerate() {
-            let checks = &rules[*value];
-
-            for i in index+1..update.len() {
-                let ahead = &update[i];
-
-                if !checks.contains(ahead) {
-                    println!("Failed, update {:?} had problem with {:?}", update,ahead);
-                    continue 'update;
-                }
-            }
-        }
-        // Update is valid
-        println!("Update was valid {:?}", update);
-        sum += update[update.len() /2];
-    }
-
-    return sum;
-
-}
+    let (checks, updates)= input.split_once("\n\n").unwrap();
     
+    let mut rules = vec![Vec::new(); 100];
+
+    for check in checks.lines(){
+        let (x,y) = check.split_once("|").unwrap();
+        
+        let x = x.parse::<usize>().expect("Failed to parse number");
+        let y = y.parse::<usize>().expect("Failed to parse number");
+
+        rules[x].push(y);
+        
+    };
+        
+    let updates = updates.lines().map(|s| s.split(",").map(|x| x.parse::<usize>().unwrap()).collect::<Vec<usize>>()).collect::<Vec<Vec<usize>>>();
+
+    (rules, updates)
+}
 
 pub fn part_one(input: &str) -> Option<usize> {
     let (rules, updates) = parse(input);
 
-    print!("rules {:?} \n updates: {:?}", rules, updates);
-    Some(check_updates(&rules, updates))
+    let compare = | x: &usize, y: &usize| !rules[*y].contains(x);
 
+    let mut count = 0;
+    for update in updates {
+        if update.is_sorted_by(compare) {
+            count += update[update.len() / 2];
+        }
+    }
+
+    Some(count)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let (rules, mut updates) = parse(input);
+
+    let compare = | x: &usize, y: &usize| {
+        if rules[*x].contains(y) {
+            Ordering::Less
+        } else if rules[*y].contains(x) {
+            Ordering::Greater
+        } else {
+            Ordering::Equal
+        }
+    };
+
+    let mut count = 0;
+    for mut update in updates {
+        if !update.is_sorted_by(|x, y| compare(x,y) != Ordering::Greater) {
+            update.sort_by(compare);
+            count += update[update.len() / 2];
+        }
+    }
+
+    Some(count)
+
 }
 
 #[cfg(test)]
@@ -77,6 +77,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
