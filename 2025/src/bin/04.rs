d8a5660e9f541e21889912c7c2be036a95a59b5c
zr <iamziarehman@gmail.com>
2025-12-28 00:52:15 +0000

    2025-04.2

--- 2025/src/bin/04.rs
+++ 2025/src/bin/04.rs
@@ -1,13 +1,19 @@
 advent_of_code::solution!(4);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let input: Vec<&[u8]> = input
+    let input: Vec<Vec<u8>> = input
         .lines()
-        .map(|l| l.as_bytes()) // Use bytes instead of char because it's faster
+        .map(|l| l.as_bytes().to_vec()) // Use bytes instead of char because it's faster
         .collect();
 
     let data = input;
 
+    let accessible_rolls = calculate_accessible_rolls(&data);
+
+    Some(accessible_rolls)
+}
+
+fn calculate_accessible_rolls(data: &Vec<Vec<u8>>) -> u64 {
     let mut accessible_rolls = 0;
 
     let m = data.len();
@@ -24,11 +30,12 @@ pub fn part_one(input: &str) -> Option<u64> {
             }
         }
     }
+    accessible_rolls
+    }
 
-    Some(accessible_rolls)
-}
 
-fn is_accessible_roll(data: &Vec<&[u8]>, row: usize, column: usize) -> bool {
+
+fn is_accessible_roll(data: &Vec<Vec<u8>>, row: usize, column: usize) -> bool {
     static DELTAS: &[(isize, isize); 8] = &[
         (-1, -1),
         (0, -1),
@@ -63,8 +70,41 @@ fn is_accessible_roll(data: &Vec<&[u8]>, row: usize, column: usize) -> bool {
     true
 }
 
+fn prune_accessible_rolls(data: &mut Vec<Vec<u8>>) -> u64 {
+    let mut accessible_rolls = 0;
+
+    let m = data.len();
+    let n = data[0].len();
+
+    for row in 0..m {
+        for column in 0..n {
+            if data[row][column] != b'@' {
+                continue;
+            }
+
+            if is_accessible_roll(&data, row, column) {
+                data[row][column] = b'.';
+                accessible_rolls += 1;
+            }
+        }
+    }
+
+    return accessible_rolls
+
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut data: Vec<Vec<u8>> = input
+        .lines()
+        .map(|l| l.as_bytes().to_vec()) // Use bytes instead of char because it's faster
+        .collect();
+
+    let mut pruned_rolls = 0;
+    while calculate_accessible_rolls(&data) > 0 {
+        pruned_rolls += prune_accessible_rolls(&mut data);
+    }
+
+    Some(pruned_rolls)
 }
 
 #[cfg(test)]
@@ -80,6 +120,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(43));
     }
 }
